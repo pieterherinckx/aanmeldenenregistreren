158b483c96a74ddebfaf8668b93cfb41430270a9
Pieter Herinckx <pieterherinckx@katienen.lan>
2019-11-25 13:33:13 +0100

    Create conn.inc.php

---
+++ conn.inc.php
@@ -0,0 +1,7 @@
+<?php
+  $db_servernaam = "localhost";
+  $db_gebruikersnaam = "pieterherinckx";
+  $db_wachtwoord = "pieter";
+  $db_databank = "pieterherinckx";
+  $conn = mysqli_connect($db_servernaam, $db_gebruikersnaam, $db_wachtwoord, $db_databank);
+?>
