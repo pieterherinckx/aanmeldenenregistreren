cce2d054a398f8bd6146cb1a86e032bf7352c2b9
pieterherinckx <pieterherinckx@atheneumtienen.be>
2020-11-14 09:33:17 +0100

    Update conn.inc.php

--- conn.inc.php
+++ conn.inc.php
@@ -1,6 +1,6 @@
 <?php
   $db_servernaam = "localhost";
-  $db_gebruikersnaam = "pieterherinckx";
-  $db_wachtwoord = "pieter";
-  $db_databank = "pieterherinckx";
+  $db_gebruikersnaam = "root";
+  $db_wachtwoord = "";
+  $db_databank = "pieter";
   $conn = mysqli_connect($db_servernaam, $db_gebruikersnaam, $db_wachtwoord, $db_databank);
