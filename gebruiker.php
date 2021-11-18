f034bfcfccff27389956da5066d6709c4990f773
pieterherinckx <pieterherinckx@atheneumtienen.be>
2021-11-18 14:30:01 +0100

    Update gebruiker.php

--- gebruiker.php
+++ gebruiker.php
@@ -1,152 +1,226 @@
 <?php
-  include_once("conn.inc.php");
   session_start();
+  include_once('inc/conn.inc.php');
 ?>
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!doctype html>
+<html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
   </head>
   <body>
-    <?php
-    // registreren
-    if(isset($_GET['actie']) && $_GET['actie']=="registreren"){
-      $voornaam = $_POST['voornaam'];
-      $familienaam = $_POST['familienaam'];
-      $gebruikersnaam = $_POST['gebruikersnaam'];
-      $wachtwoord = md5($_POST['wachtwoord']);
-      $wachtwoord_controle = md5($_POST['wachtwoord_controle']);
-      $email = $_POST['email'];
+    <div class="container">
+      <?php
+      //profiel aanpassen
+      if(isset($_GET['actie']) && $_GET['actie']=="aanpassen"){
+        $klant_id = $_SESSION['klant_id'];
+        $voornaam = $_POST['voornaam'];
+        $familienaam = $_POST['familienaam'];
+        $gebruikersnaam = $_POST['gebruikersnaam'];
+        $email = $_POST['email'];
+        $wachtwoord = md5($_POST['wachtwoord']);
+        $controle_wachtwoord = md5($_POST['controle_wachtwoord']);
 
-      echo "<h1>Registreren</h1>" ;
-      if($wachtwoord != $wachtwoord_controle){
-        echo "De wachtwoorden komen niet overeen";
-      }
-      else {
-        $query = "INSERT INTO gebruiker(voornaam, familienaam, gebruikersnaam, wachtwoord, email) VALUES ('$voornaam', '$familienaam', '$gebruikersnaam', '$wachtwoord', '$email');";
-        mysqli_query($conn, $query);
-      }
-    }
-
-    // registreerformulier
-    else if(isset($_GET['actie']) && $_GET['actie']=="registreerformulier"){
-      echo "<h1>Registreerformulier</h1>" ;
-      echo "
-          <form action='?actie=registreren' method='post'>
-            <label for='voornaam'>Voornaam</label>
-            <input type='text' name='voornaam' value=''>
-            <label for='familienaam'>Familienaam</label>
-            <input type='text' name='familienaam' value=''>
-            <label for='gebruikersnaam'>Gebruikersnaam</label>
-            <input type='text' name='gebruikersnaam' value=''>
-            <label for='wachtwoord'>Wachtwoord</label>
-            <input type='password' name='wachtwoord' value=''>
-            <label for='wachtwoord_controle'>Wachtwoord controle</label>
-            <input type='password' name='wachtwoord_controle' value=''>
-            <label for='email'>Email</label>
-            <input type='text' name='email' value=''>
-            <button type='submit' name='registreren'>Registreren</button>
-          </form>";
-    }
-
-    // bewerken
-    else if(isset($_GET['actie']) && $_GET['actie']=="bewerken"){
-      $gebruikersnr = $_POST['gebruikersnr'];
-      $voornaam = $_POST['voornaam'];
-      $familienaam = $_POST['familienaam'];
-      $email = $_POST['email'];
-
-      $query = "UPDATE gebruiker SET voornaam = '$voornaam', familienaam = '$familienaam', email = '$email' WHERE gebruikersnr = $gebruikersnr;";
-      mysqli_query($conn, $query);
-
-      echo "Uw gegevens zijn aangepast";
-    }
-
-    // bewerkformulier
-    else if(isset($_GET['actie']) && $_GET['actie']=="bewerkformulier"){
-      $gebruikersnaam = $_SESSION['gebruikersnaam'];
-      $query = "SELECT * FROM gebruiker WHERE gebruikersnaam = '$gebruikersnaam';";
-      $resultaat = mysqli_query($conn, $query);
-      $rij = mysqli_fetch_assoc($resultaat);
-
-      $gebruikersnr = $rij['gebruikersnr'];
-      $voornaam = $rij['voornaam'];
-      $familienaam = $rij['familienaam'];
-      $email = $rij['email'];
-
-      echo "<h1>Bewerkformulier</h1>";
-      echo "
-          <form action='?actie=bewerken' method='post'>
-            <input type='hidden' name='gebruikersnr' value='$gebruikersnr'>
-            <label for='voornaam'>Voornaam</label>
-            <input type='text' name='voornaam' value='$voornaam'>
-            <label for='familienaam'>Familienaam</label>
-            <input type='text' name='familienaam' value='$familienaam'>
-            <label for='email'>Email</label>
-            <input type='text' name='email' value='$email'>
-            <button type='submit' name='bewerken'>Bewerken</button>
-          </form>";
-    }
-
-    // verwijderen
-    else if(isset($_GET['actie']) && $_GET['actie']=="verwijderen"){
-      $gebruikersnaam = $_SESSION['gebruikersnaam'];
-      $query = "DELETE FROM gebruiker WHERE gebruikersnaam = '$gebruikersnaam';";
-
-      mysqli_query($conn, $query);
-      session_destroy();
-    }
-
-    // afmelden
-    else if(isset($_GET['actie']) && $_GET['actie']=="afmelden"){
-      session_destroy();
-      echo "U bent nu afgemeld";
-    }
-
-    // aanmelden
-    else if(isset($_GET['actie']) && $_GET['actie']=="aanmelden"){
-      $gebruikersnaam = $_POST['gebruikersnaam'];
-      $wachtwoord = md5($_POST['wachtwoord']);
-
-      $query = "SELECT gebruikersnr FROM gebruiker WHERE gebruikersnaam = '$gebruikersnaam' AND wachtwoord = '$wachtwoord';";
-
-      $resultaat = mysqli_query($conn, $query);
-      $resultaatCheck = mysqli_num_rows($resultaat);
-
-      if($resultaatCheck > 0){
-        echo "U bent aangemeld";
-        $_SESSION['aangemeld'] = true;
-        $_SESSION['gebruikersnaam'] = $gebruikersnaam;
-      }
-      else {
-        echo "Gegevens kloppen niet";
+        if($wachtwoord != $controle_wachtwoord){
+          echo "
+          <div class='alert alert-danger' role='alert'>
+            De wachtwoorden komen niet overeen!
+          </div>";
+        }
+        else {
+          $query = "UPDATE tbl_klant SET voornaam = '$voornaam', familienaam= '$familienaam', gebruikersnaam = '$gebruikersnaam', email = '$email', wachtwoord= '$wachtwoord' WHERE klant_id = $klant_id";
+          //echo $query;
+          mysqli_query($conn,$query);
+          echo "
+          <div class='alert alert-success' role='alert'>
+            De gegevens zijn aangepast!
+          </div>";
+          $_SESSION['voornaam'] = $voornaam;
+          $_SESSION['familienaam'] = $familienaam;
+          $_SESSION['gebruikersnaam'] = $gebruikersnaam;
+          $_SESSION['email'] = $email;
+        }
       }
 
-    }
 
-    // aanmeldformulier  --> STANDAARD
-    else {
-      if(isset($_SESSION['aangemeld']) && $_SESSION['aangemeld']){
-        echo "<p>Hallo ".$_SESSION['gebruikersnaam'].". U bent al aangemeld</p>";
-        echo "<a href='?actie=afmelden'>Afmelden</a> - ";
-        echo "<a href='?actie=bewerkformulier'>Profiel bewerken</a> - ";
-        echo "<a href='?actie=verwijderen'>Profiel verwijderen</a>";
-      }
-      else {
-        echo "<h1>Aanmeldformulier</h1>" ;
+      // profiel weergeven
+      else if(isset($_GET['actie']) && $_GET['actie']=="profiel"){
+        $voornaam = $_SESSION['voornaam'];
+        $familienaam = $_SESSION['familienaam'];
+        $gebruikersnaam = $_SESSION['gebruikersnaam'];
+        $email = $_SESSION['email'];
+        $actief = $_SESSION['actief'];
         echo "
-            <form action='?actie=aanmelden' method='post'>
-              <label for='gebruikersnaam'>Gebruikersnaam</label>
-              <input type='text' name='gebruikersnaam' value=''>
-              <label for='wachtwoord'>Wachtwoord</label>
-              <input type='password' name='wachtwoord' value=''>
-              <button type='submit' name='aanmelden'>Aanmelden</button>
-            </form>";
-
-        echo "<a href='?actie=registreerformulier'>Registreer</a>";
+        <h1>Profiel</h1>
+        <form action='?actie=aanpassen' method='post'>
+          <div class='mb-3'>
+            <label for='voornaam' class='form-label'>Voornaam:</label>
+            <input type='text' name='voornaam' class='form-control' value='$voornaam'>
+          </div>
+          <div class='mb-3'>
+            <label for='familienaam' class='form-label'>Familienaam:</label>
+            <input type='text' name='familienaam' class='form-control' value='$familienaam'>
+          </div>
+          <div class='mb-3'>
+            <label for='gebruikersnaam' class='form-label'>Gebruikersnaam:</label>
+            <input type='text' name='gebruikersnaam' class='form-control' value='$gebruikersnaam'>
+          </div>
+          <div class='mb-3'>
+            <label for='email' class='form-label'>Email:</label>
+            <input type='email' name='email' class='form-control' value='$email'>
+          </div>
+          <div class='mb-3'>
+            <label for='wachtwoord' class='form-label'>Wachtwoord:</label>
+            <input type='password' name='wachtwoord' class='form-control' placeholder='Vul hier je nieuw wachtwoord in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='controle_wachtwoord' class='form-label'>Controle wachtwoord:</label>
+            <input type='password' name='controle_wachtwoord' class='form-control' placeholder='Vul hier je nieuw controle wachtwoord in: '>
+          </div>
+          <button type='submit' class='btn btn-primary'>Aanpassen</button>
+        </form>";
       }
-    }
-    ?>
+
+
+      // afmelden verwerken
+      else if(isset($_GET['actie']) && $_GET['actie']=="afmelden"){
+        session_destroy();
+        echo "
+        <div class='alert alert-success' role='alert'>
+          De gebruiker is afgemeld!
+        </div>";
+      }
+
+      //registratieformulier verwerken - registreren
+      else if(isset($_GET['actie']) && $_GET['actie']=="registreren"){
+        $voornaam = $_POST['voornaam'];
+        $familienaam = $_POST['familienaam'];
+        $gebruikersnaam = $_POST['gebruikersnaam'];
+        $email = $_POST['email'];
+        $wachtwoord = md5($_POST['wachtwoord']);
+        $controle_wachtwoord = md5($_POST['controle_wachtwoord']);
+
+        if($wachtwoord != $controle_wachtwoord){
+          echo "
+          <div class='alert alert-danger' role='alert'>
+            De wachtwoorden komen niet overeen!
+          </div>";
+        }
+        else {
+          //echo $voornaam." ".$familienaam." ".$gebruikersnaam." ".$email." ".$wachtwoord." ".$controle_wachtwoord;
+
+          $query = "INSERT INTO tbl_klant(voornaam, familienaam, gebruikersnaam, email, wachtwoord) VALUES ('$voornaam', '$familienaam', '$gebruikersnaam', '$email', '$wachtwoord')";
+          //echo $query;
+          mysqli_query($conn,$query);
+        }
+      }
+
+      //registratieformulier tonen
+      else if(isset($_GET['actie']) && $_GET['actie']=="registratieformulier"){
+        echo "
+        <h1>Registratieformulier</h1>
+        <form action='?actie=registreren' method='post'>
+          <div class='mb-3'>
+            <label for='voornaam' class='form-label'>Voornaam:</label>
+            <input type='text' name='voornaam' class='form-control' placeholder='Vul hier je voornaam in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='familienaam' class='form-label'>Familienaam:</label>
+            <input type='text' name='familienaam' class='form-control' placeholder='Vul hier je familienaam in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='gebruikersnaam' class='form-label'>Gebruikersnaam:</label>
+            <input type='text' name='gebruikersnaam' class='form-control' placeholder='Vul hier je gebruikersnaam in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='email' class='form-label'>Email:</label>
+            <input type='email' name='email' class='form-control' placeholder='Vul hier je email in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='wachtwoord' class='form-label'>Wachtwoord:</label>
+            <input type='password' name='wachtwoord' class='form-control' placeholder='Vul hier je wachtwoord in: '>
+          </div>
+          <div class='mb-3'>
+            <label for='controle_wachtwoord' class='form-label'>Controle wachtwoord:</label>
+            <input type='password' name='controle_wachtwoord' class='form-control' placeholder='Vul hier je controle wachtwoord in: '>
+          </div>
+          <button type='submit' class='btn btn-primary'>Registreren</button>
+        </form>";
+      }
+
+      //aanmeldformulier verwerken - aanmelden
+      else if(isset($_GET['actie']) && $_GET['actie']=="aanmelden"){
+        $gebruikersnaam = $_POST['gebruikersnaam'];
+        $wachtwoord = md5($_POST['wachtwoord']);
+
+        $query = "SELECT * FROM tbl_klant WHERE gebruikersnaam = '$gebruikersnaam' AND wachtwoord = '$wachtwoord'";
+        $result = mysqli_query($conn, $query);
+        $numRows = mysqli_num_rows($result);
+
+        // combinatie gebruikersnaam en wachtwoord bestaat niet
+        if($numRows == 0){
+          echo "
+          <div class='alert alert-danger' role='alert'>
+            De combinatie gebruikersnaam en wachtwoord bestaat niet!
+          </div>";
+        }
+        // de gebruiker is aangemeld
+        else {
+          $_SESSION['aangemeld']=true;
+
+          // de associatieve array van het resultaat ophalen
+          $row = mysqli_fetch_assoc($result);
+          $_SESSION['klant_id'] = $row['klant_id'];
+          $_SESSION['voornaam'] = $row['voornaam'];
+          $_SESSION['familienaam'] = $row['familienaam'];
+          $_SESSION['gebruikersnaam'] = $row['gebruikersnaam'];
+          $_SESSION['email'] = $row['email'];
+          $_SESSION['actief'] = $row['actief'];
+          echo "
+          <div class='alert alert-success' role='alert'>
+            De gebruiker is aangemeld!
+          </div>";
+        }
+
+      }
+
+      // DEFAULT: aanmeldformulier tonen
+      else {
+        // controleren of de gebruiker al aangemeld is dus aanmeldformulier niet meer tonen
+        if(isset($_SESSION['aangemeld']) && $_SESSION['aangemeld']){
+          echo "<a href='?actie=afmelden'>Afmelden</a> <br>";
+          echo "<a href='?actie=profiel'>Profiel</a>";
+        }
+        // gebruiker is nog niet aangemeld dus aanmeldformulier tonen
+        else {
+          echo "
+          <h1>aanmeldformulier</h1>
+          <form action='?actie=aanmelden' method='post'>
+            <div class='mb-3'>
+              <label for='gebruikersnaam' class='form-label'>Gebruikersnaam:</label>
+              <input type='text' name='gebruikersnaam' class='form-control' placeholder='Vul hier je gebruikersnaam in: '>
+            </div>
+            <div class='mb-3'>
+              <label for='wachtwoord' class='form-label'>Wachtwoord:</label>
+              <input type='password' name='wachtwoord' class='form-control' placeholder='Vul hier je wachtwoord in: '>
+            </div>
+            <button type='submit' class='btn btn-primary'>Aanmelden</button>
+            <a href='?actie=registratieformulier'>Registreren</a>
+          </form>";
+        }
+      }
+
+
+      ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
   </body>
 </html>
